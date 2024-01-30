d32b9234608cce59bde5ddc6e99e96316368967e
Dharitri-org <contact@dharitri.org>
2024-01-30 12:14:40 +0530

    Added additional data in LogEntryApi

--- core/constants.go
+++ core/constants.go
@@ -267,7 +267,7 @@ const GasRefundForRelayerMessage = "gas refund for relayer"
 // InitialVersionOfTransaction defines the initial version for a transaction
 const InitialVersionOfTransaction = uint32(1)
 
-// DefaultAddressPrefix is the default hrp of Dharitri/Dharitri
+// DefaultAddressPrefix is the default hrp of Dharitri/Andes
 const DefaultAddressPrefix = "moa"
 
 // TopicRequestSuffix represents the topic name suffix for requests
